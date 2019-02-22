dd1d4d0f7bf7a494dfd356440466c6754916d7ba
Sulekha <sulekha.developer@gmail.com>
2019-02-22 10:55:10 +0000

    Autocompletion List is added to the Username field. When new string is typed in the Username field it gets added to the autocompletion list.

--- app/src/main/java/com/training/simpleloginform/MainActivity.java
+++ app/src/main/java/com/training/simpleloginform/MainActivity.java
@@ -2,24 +2,37 @@ package com.training.simpleloginform;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Assignment add validation to edit text fields
  * <p>
  * if an edit text is empty, show a toast that says "edit text cannot be empty", where edit text can be username or password
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
 
-    private EditText usernameEditText;
+    private AutoCompleteTextView usernameEditText;
     private EditText passwordEditText;
     private Button submitButton;
     private TextView usernameDetailsTextView;
     private TextView passwordDetailsTextView;
     private Button resetButton;
+
+    List<String> names = new ArrayList<>();
+    Set<String> namesList = new HashSet<>();
 
 
     @Override
@@ -34,8 +47,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submitButton = findViewById(R.id.submit_button);
         resetButton = findViewById(R.id.reset_button);
 
+
+            names.add("Desmond");
+            names.add("Dean");
+            names.add("Iliana");
+            names.add("Sulekha");
+
+            usernameEditText.addTextChangedListener(this);
+            usernameEditText.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, names));
+
         submitButton.setOnClickListener(this);
         resetButton.setOnClickListener(this);
+
     }
 
 
@@ -48,10 +71,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 usernameDetailsTextView.setText(username);
                 passwordDetailsTextView.setText(password);
+
+                names.add(username);
+                namesList.addAll(names);
+                names.clear();
+                names.addAll(namesList);
+
                 break;
             case R.id.reset_button:
                 setResetButton();
-
                 break;
         }
     }
@@ -61,6 +89,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         passwordEditText.setText("");
         usernameDetailsTextView.setText("");
         passwordDetailsTextView.setText("");
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        usernameEditText.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, names));
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
 
     }
 }
