380c5215e1d8c0f0eb11b1f49691721263cbd426
Iliana <ilianabezini@yahoo.co.uk>
2019-02-22 11:06:28 +0000

    send user login details to second activity

--- app/src/main/java/com/training/simpleloginform/MainActivity.java
+++ app/src/main/java/com/training/simpleloginform/MainActivity.java
@@ -1,11 +1,11 @@
 package com.training.simpleloginform;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * Assignment add validation to edit text fields
@@ -17,8 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button submitButton;
-    private TextView usernameDetailsTextView;
-    private TextView passwordDetailsTextView;
     private Button resetButton;
 
 
@@ -26,11 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         usernameEditText = findViewById(R.id.user_name_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
-        usernameDetailsTextView = findViewById(R.id.username_details_textview);
-        passwordDetailsTextView = findViewById(R.id.password_details_textview);
         submitButton = findViewById(R.id.submit_button);
         resetButton = findViewById(R.id.reset_button);
 
@@ -38,16 +33,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetButton.setOnClickListener(this);
     }
 
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.submit_button:
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+                Intent i = new Intent(this, SecondActivity.class);
+                i.putExtra("username", usernameEditText.getText().toString());
+                i.putExtra("password", passwordEditText.getText().toString());
+                startActivity(i);
 
-                usernameDetailsTextView.setText(username);
-                passwordDetailsTextView.setText(password);
+
                 break;
             case R.id.reset_button:
                 setResetButton();
@@ -59,8 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void setResetButton() {
         usernameEditText.setText("");
         passwordEditText.setText("");
-        usernameDetailsTextView.setText("");
-        passwordDetailsTextView.setText("");
 
     }
+
 }
